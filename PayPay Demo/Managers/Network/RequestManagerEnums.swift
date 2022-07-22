1d5a5e9c3ad9dfb07c182ef2840a9dabc6ae1c2f
Leon <mrtonmoy12@gmail.com>
2022-07-23 00:19:21 +0600

    -- added request manager enum

--- PayPay Demo/Managers/Network/RequestManagerEnums.swift
+++ PayPay Demo/Managers/Network/RequestManagerEnums.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+enum RequestType: String, CaseIterable {
+    case get    = "GET"
+    case post   = "POST"
+}
+
+enum ErrorType: Error {
+    case noInternet
+    case networkProblem
+    case unknownError(String)
+    case errorDescription(Error)
+}
+
+enum ParameterType {
+    case query
+    case queryAndBody
+    case path
+    case pathAndBody
+    case all
+}
